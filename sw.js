3173565ff37ba7d6058f51532cf832140c55ba7a
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-06-20 03:44:30 +0200

    feat: added leaflet js/css links to sw.

--- sw.js
+++ sw.js
@@ -21,7 +21,9 @@ self.addEventListener('install', function (event) {
                 "/img/8.jpg",
                 "/img/9.jpg",
                 "/img/10.jpg",
-                "/data/restaurants.json"
+                "/data/restaurants.json",
+                "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+                "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
             ]);
         })
     );
