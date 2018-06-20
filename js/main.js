07c6321907b8bceb774b8c4274d650782e1b2109
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-06-20 02:37:25 +0200

    feat: added / for sw folders/files paths.

--- js/main.js
+++ js/main.js
@@ -210,7 +210,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 } */
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', {
+  navigator.serviceWorker.register('/sw.js', {
     scope: '/'
   }).then(function () {
     console.log('serviceWorker registered successfully');
